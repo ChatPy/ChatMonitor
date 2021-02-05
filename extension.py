fa1e31e0d338dadf3fe693e6be24cf3938dfc7a5
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-02-05 11:07:59 -0600

    Update extension.py

--- extension.py
+++ extension.py
@@ -1,1 +1,19 @@
 # UNDER DEVELOPMENT
+
+# Take this as a example if you want to write your own extension.
+# The extension wiki will be released soon.
+
+import threading
+template_file = open("template.html")
+global template
+global recordings
+template = template_file.read()
+recordings = []
+def record(message):
+  recordings.append(message)
+def extension():
+  length = 0
+  while True:
+    if length != len(recordings):
+      new_data = recordings[-1]
+      # UNDER DEVELOPMENT HERE
